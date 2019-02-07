bfc722b7456d9603d745805a417280a96a2d5ead
Madhanmyil <47316868+Madhanmyil@users.noreply.github.com>
2019-02-07 14:54:04 +0530

    Add files via upload

---
+++ 3.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int a=5/2;
+		float b=5f
+		float c=5f/2f;
+		double d=5d;
+		System.out.println("intvalue="+a);
+		System.out.println("intvalue="+b);
+		System.out.println("intvalue="+c);
+		System.out.println("intvalue="+d);
+	}
+}
+
